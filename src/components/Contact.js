0801c26a900b2d81c85ccdc35c89726ceb99be0b
Shubham Singh <86901734+shubhamsingh819@users.noreply.github.com>
2023-07-28 14:09:15 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+const Contact = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ color: "blue" }}>Contact Us Page</h1>
+      <h2>Name:Shubham Singh</h2>
+      <h4>Email:shubhamsingh8199@gmail.com</h4>
+      <h3>Mobile:9928707466</h3>
+      <h4 style={{ color: "red" }}>
+        note: Contact us for creating fullStack website , Mobile App , FrontEnd
+        Ui and manyMore.
+      </h4>
+    </div>
+  );
+};
+export default Contact;
